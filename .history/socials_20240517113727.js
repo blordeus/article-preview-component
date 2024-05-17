9959d2869d7ca7417c62f9270e52183e0ac7f783
blordeus <imaginedbybryan@gmail.com>
2024-05-17 17:03:09 -0400

    "Updated index.html and style.css files: added script tag, modified HTML structure, and added CSS styles for font, layout, and images."

---
+++ .history/socials_20240517113727.js
@@ -0,0 +1,3 @@
+const tooltipTriggerList = doconst tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))cument.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
